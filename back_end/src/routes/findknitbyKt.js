d29e5cd799438d67064546d9020e7dbe011e2d5f
Rabia Bezzaz <r.bezzaz@yahoo.fr>
2023-06-03 19:30:24 +0200

    modif appel get

--- back_end/src/routes/findknitbyKt.js
+++ back_end/src/routes/findknitbyKt.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.get('/api/knits/:id', (req, res) => {
     Knit.findByPk(req.params.id)
       .then(knit => {
-        const message = 'Un pokémon a bien été trouvé.'
+        const message = 'Un point a bien été trouvé.'
         res.json({ message, data: knit })
       })
   })
